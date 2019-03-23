4038ee1a9474ee57d815bf847ed37d8bf40ee5ec
= <=>
2019-03-23 17:31:15 -0400

    added nba teams and script to create folders and parse out the games for each team of that season

--- nba/getteamstats.py
+++ nba/getteamstats.py
@@ -1,19 +1,34 @@
 import pandas as pd
-import os
+import requests, pickle, os
+from bs4 import BeautifulSoup
+
+#direct path to user directory
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def ensure_dir(folder):
 	# directory = os.path.dirname(file_path)
 	if not os.path.exists(folder):
 		os.makedirs(folder)
 
-#direct path to user directory
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-
 def grabTeamData(team, year):
-	print('grabbing %s with the year %s' % (team, year))
-	pass
+	games = []
 
+	request_url = ("https://www.basketball-reference.com/teams/%s/%s.html" %(team, year))
+	response = requests.get(request_url)
+	soup = BeautifulSoup(response.content, 'html.parser')
+	#Finding the tag associated with all the games played for the team for that season
+	timeline = soup.find(id = "timeline_results")
+
+	base_url = "https://www.basketball-reference.com"
+
+	#grab all the links for the games
+	for day in timeline.findAll('a'):
+	    link = day.get('href')
+	    
+	    if link:
+	        games.append(base_url + link)
+
+	return games
 
 def populateFolders(file):
 	df = pd.read_excel(file, sheet_name = 'Sheet1')
@@ -33,16 +48,24 @@ def populateFolders(file):
 
 		#Create new dir for years of the teams
 		teamYear = df['Year'][i]
-		ensure_dir(teamYear)
+		ensure_dir(str(teamYear))
 
-		grabTeamData(teamName, teamYear)
+		yeardir = teamdir + "\\" + str(teamYear)
+		os.chdir(yeardir)
+
+		# for num in range(1,83):
+		# 	ensure_dir("G" + str(num))
+		games = grabTeamData(teamName, teamYear)
+		with open('games_link.txt', 'a') as f:
+			f.write('\n'.join(games))
 
 		#return to main dir
 		os.chdir(dir_path)
 
 def main():
 	filename = 'NBATeams.xlsx'
-
 	populateFolders(filename)
+
+
 
 main()
