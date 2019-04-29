e637bf90fb2abb7e2add75fa233caa9d2ce774ee
Ted.C <ted0697@gmail.com>
2019-04-29 13:04:00 -0400

    added elo evaluator... need to finish grabbing elo of all teams

--- nba/BOS1986/aggregateData.py
+++ nba/BOS1986/aggregateData.py
@@ -59,14 +59,14 @@ def getMarginOfVictory(json, goatTeam, goatElo):
 	if t1.team != goatTeam:
 		t1, t2 = t2, t1
 
-	print(t1, ' goat team')
-	print(t2, ' other team')
-	print('-------------')
+	# print(t1, ' goat team')
+	# print(t2, ' other team')
+	# print('-------------')
 
 	#returns margin of victory, whether team won or lost, and elo diff..
 	if t1.home:
-		return (int(t1.score) - 3) - int(t2.score), int(t1.score) > int(t2.score), 1500
-	return int(t1.score)  - int(t2.score), int(t1.score) > int(t2.score), 1500
+		return (int(t1.score) - 3) - int(t2.score), int(t1.score) > int(t2.score), 1500 - 100
+	return int(t1.score)  - int(t2.score), int(t1.score) > int(t2.score), 1500 + 100
 
 def getMarginOfVictory2(json, goatTeam, goatElo):
 
@@ -80,8 +80,6 @@ def getMarginOfVictory2(json, goatTeam, goatElo):
 	18:1200
 	}
 
-	print(elo)
-
 	Team = namedtuple('Team' , ['team', 'score', 'record', 'home'])
 
 	t1 = Team(json[0]['TEAM'], json[0]['STATS']['BASIC']['PTS'], json[0]['RECORD'], json[0]['HOME'])
@@ -90,14 +88,46 @@ def getMarginOfVictory2(json, goatTeam, goatElo):
 	if t1.team != goatTeam:
 		t1, t2 = t2, t1
 
-	print(t1, ' goat team')
-	print(t2, ' other team')
-	print('-------------')
+	# print(t1, ' goat team')
+	# print(t2, ' other team')
+	# print('-------------')
+
+	#get the record of the team
+	oppoRecord = t2.record.split('-')
+	oppWins, oppLoses = int(oppoRecord[0]), int(oppoRecord[1])
+	winrate = oppWins/(oppWins+oppLoses) * 100
+
+	# print(winrate, ' ---- winrate')
+	closest = None
+	difference = 10000000
+	for num in [80, 70, 60, 50, 37, 26, 18]:
+		if abs(num - winrate) < difference:
+			closest = num
+			difference = abs(num - winrate)
+
+
+	# print(elo[closest], ' -- calculated elo')	
+
+
+	#calculate opponent elo
+	oppElo = elo[closest]
 
 	#returns margin of victory, whether team won or lost, and elo diff..
 	if t1.home:
-		return (int(t1.score) - 3) - int(t2.score), int(t1.score) > int(t2.score), 1500
-	return int(t1.score)  - int(t2.score), int(t1.score) > int(t2.score), 1500
+		return (int(t1.score) - 3) - int(t2.score), int(t1.score) > int(t2.score), oppElo - 100
+	return int(t1.score)  - int(t2.score), int(t1.score) > int(t2.score), oppElo + 100
+
+def calculateExpected(goatElo, oppElo):
+
+	diff = (oppElo - goatElo)/400
+	res = (1 / (1 + (10**diff)))
+
+	return res
+
+def calculateK(marginWin, eloDiff):
+	numerator = (marginWin + 3)**0.8
+	denom = 7.5 + (0.006*eloDiff)
+	return 20*(numerator/denom)
 		
 def evaluateElo():
 	elo = [1500]
@@ -110,10 +140,26 @@ def evaluateElo():
 			jsonFile = json.load(jsonFile)
 
 			#first 10 games... everyone is 1500 to stablize elo first.
-			if num < 9:
+			if num < 10:
 				margin, goatWon, oppElo = getMarginOfVictory(jsonFile, goatTeam, 0)
 			else:
 				margin, goatWon, oppElo = getMarginOfVictory2(jsonFile, goatTeam, 0)
+
+
+
+			expectedWin = calculateExpected(elo[num], oppElo)
+			eloDiff = 0
+			if goatWon:
+				eloDiff = elo[num] - oppElo
+			else:
+				eloDiff = oppElo - elo[num]
+
+			calculatedK = calculateK(abs(margin), eloDiff)
 			
+			newElo = calculatedK*(goatWon - expectedWin) + elo[num]
+			elo.append(int(newElo))
+			print(int(newElo), ' --- new elo')
+			print(' ------- ')
+	print(len(elo))
 
 evaluateElo()
