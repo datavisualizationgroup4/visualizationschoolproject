c67fb535b5fbcce228632400f04368fcee173af0
Arikuzzaman Idrisy <arik.idrisy@gmail.com>
2019-04-30 01:22:12 -0400

    Made ScatterPlot w/ Menus for Each Team
    Need team to check for errors on the Vis with the Top bar and what not

---
+++ arik_plots/teamVisSM/dropdownMenu.js
@@ -0,0 +1,30 @@
+/*
+<select>
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+</select>
+*/
+
+export const dropdownMenu = (selection, props) => {
+  const {
+    options,
+    onOptionClicked,
+    selectedOption
+  } = props;
+  
+  let select = selection.selectAll('select').data([null]);
+  select = select.enter().append('select')
+    .merge(select)
+      .on('change', function() {
+        onOptionClicked(this.value);
+      });
+  
+  const option = select.selectAll('option').data(options);
+  option.enter().append('option')
+    .merge(option)
+      .attr('value', d => d)
+      .property('selected', d => d === selectedOption)
+      .text(d => d);
+};
